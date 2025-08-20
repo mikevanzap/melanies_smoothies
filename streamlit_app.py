d2ff169161336f1cb0390df5e4d2c508956852ea
mikevanzap <68391619+mikevanzap@users.noreply.github.com>
2025-08-20 17:35:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,8 @@ if time_to_insert:
 
  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response)
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     
 
